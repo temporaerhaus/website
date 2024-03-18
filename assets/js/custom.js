a0ee1052bd092b04a3314b945f9fd548320c19b0
ech0 <mail@ech0.de>
2024-03-18 22:29:35 +0100

    potential fix for membership form firefox bug
    suspected issue: https://bugzilla.mozilla.org/show_bug.cgi?id=1370630

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
     nextStep.parentNode.style.display = 'flex';
     document.querySelector('button[type="submit"]').remove();
     form.children[0].style.display = 'block';
-  
+
     window.addEventListener('hashchange', () => {
       const hash = location.hash.startsWith('#step') ? location.hash : '#step1';
       const a = document.querySelector(`a[href="${hash}"]`);
@@ -78,91 +78,97 @@ document.addEventListener('DOMContentLoaded', () => {
       a.parentNode.style.fontWeight = 'bold';
       a.parentNode.style.color = 'black';
       a.style.color = 'black';
-  
+
       e.parentNode.querySelectorAll('div[id]').forEach(e => {
         e.style.display = 'none';
       });
       e.style.display = 'block';
-  
+
       prevStep.disabled = (hash === '#step1');
       nextStep.innerText = (hash === '#step5') ? '✉ Antrag Versenden' : '▶ Weiter';
     });
-  
+
     const changeStep = (e) => {
       e.preventDefault();
       e.stopPropagation();
-  
+
       const hash = location.hash.startsWith('#step') ? location.hash : '#step1';
       const target = (Number(hash.slice(5)) || 0) + (e.target.id === 'nextStep' ? 1 : -1);
       if (target === 6) {
-        form.dispatchEvent(new Event('submit'));
+        form.dispatchEvent(new Event('submit', { cancelable: true }));
         return;
       }
-  
+
       location.hash = `#step${target}`;
-      window.dispatchEvent(new Event('hashchange'));
+      window.dispatchEvent(new Event('hashchange', { cancelable: true }));
     };
     nextStep.addEventListener('click', changeStep);
     prevStep.addEventListener('click', changeStep);
-  
+
     form.addEventListener('submit', async (e) => {
       e.preventDefault();
       e.stopPropagation();
-  
+
       form.querySelectorAll('p').forEach(e => e.classList.remove('danger'));
       nextStep.disabled = true;
       prevStep.disabled = true;
-  
+
       let response = document.getElementById('formResponse')
       if (!response) {
         response = document.createElement('div');
         response.id = 'formResponse';
         form.insertAdjacentElement('beforebegin', response);
       }
-  
+
       response.className = 'info';
       response.innerHTML = document.getElementById('application-loading').innerHTML;
-  
+
       const data = new URLSearchParams(new FormData(form));
-      const res = await fetch(form.action, { method: 'post', body: data });
-      nextStep.disabled = false;
-      prevStep.disabled = false;
-  
-      const text = await res.text();
-      const parser = new DOMParser();
-      const htmlDocument = parser.parseFromString(text, 'text/html');
-      const main = htmlDocument.documentElement.querySelector('main');
-  
-      if (res.status === 200) {
-        form.reset();
-        location.hash = '#step1';
-        window.dispatchEvent(new Event('hashchange'));
-        response.className = 'success';
-        response.innerHTML = main.innerHTML;
-      } else if (res.status === 500) {
-        response.className = 'danger';
-        response.innerHTML = main.innerHTML;
-      } else {
-        const body = JSON.parse(main.dataset.errors);
-        response.className = 'danger';
-        response.innerHTML = document.getElementById('application-error').innerHTML;
-  
-        let step = 'step5';
-        for (const field of Object.keys(body)) {
-          const e = document.getElementById(field);
-          e?.closest?.('p')?.classList?.add?.('danger');
-          const id = e?.closest?.('div[id]')?.id;
-          if (id < step) {
-            step = id;
+      try {
+        const res = await fetch(form.action, { method: 'post', body: data });
+        nextStep.disabled = false;
+        prevStep.disabled = false;
+
+        const text = await res.text();
+        const parser = new DOMParser();
+        const htmlDocument = parser.parseFromString(text, 'text/html');
+        const main = htmlDocument.documentElement.querySelector('main');
+
+        if (res.status === 200) {
+          form.reset();
+          location.hash = '#step1';
+          window.dispatchEvent(new Event('hashchange', { cancelable: true }));
+          response.className = 'success';
+          response.innerHTML = main.innerHTML;
+        } else if (res.status === 500) {
+          response.className = 'danger';
+          response.innerHTML = main.innerHTML;
+        } else {
+          const body = JSON.parse(main.dataset.errors);
+          response.className = 'danger';
+          response.innerHTML = document.getElementById('application-error').innerHTML;
+
+          let step = 'step5';
+          for (const field of Object.keys(body)) {
+            const e = document.getElementById(field);
+            e?.closest?.('p')?.classList?.add?.('danger');
+            const id = e?.closest?.('div[id]')?.id;
+            if (id < step) {
+              step = id;
+            }
+          }
+          if (location.hash !== `#${step}`) {
+            location.hash = `#${step}`;
+            window.dispatchEvent(new Event('hashchange', { cancelable: true }));
           }
         }
-        if (location.hash !== `#${step}`) {
-          location.hash = `#${step}`;
-          window.dispatchEvent(new Event('hashchange'));
-        }
+      } catch (e) {
+          console.error(e);
+          response.className = 'danger';
+          response.innerHTML = 'Es ist ein unerwarteter Fehler aufgetreten 😢';
       }
     });
-  
-    window.dispatchEvent(new Event('hashchange'));
+
+    window.dispatchEvent(new Event('hashchange', { cancelable: true }));
   }
 }, false);

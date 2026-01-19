c2257b7b7f01e5676b025f99d9a3ee5592db0d96
Matthias Schneider <ms@flurp.de>
2026-01-19 22:52:44 +0100

    adds a first draft of the slider subpage of the open data group. more to come.

---
+++ content/pages/slider/index.de.md
@@ -0,0 +1,17 @@
+---
+title: Wikimedia Commons - tph Slider
+type: page
+slug: slider
+custom_css: ["css/slider.css"]
+custom_js: ["js/slider.js"]
+---
+
+# Donauabsenkung
+
+{{< slider before="Donauabsenkung_0000.jpg" after="Donauabsenkung_0001.jpg" >}}
+
+Die Donauabsenkung fotografiert mit der tph-Drohne. Hier kann noch mehr Text stehen. 
+
+# Slider 2 
+...todo
+
---
+++ static/js/slider.js
@@ -0,0 +1,83 @@
+const slider = document.getElementById('before-after-slider');
+const before = document.getElementById('before-image');
+const beforeImage = before.getElementsByTagName('img')[0];
+const resizer = document.getElementById('resizer');
+
+let active = false;
+
+document.addEventListener("DOMContentLoaded", function() {
+    let width = slider.offsetWidth;
+    beforeImage.style.width = width + 'px';
+});
+
+window.addEventListener('resize', function() {
+    let width = slider.offsetWidth;
+    beforeImage.style.width = width + 'px';
+})
+
+resizer.addEventListener('mousedown',function(){
+    active = true;
+    resizer.classList.add('resize');
+});
+
+document.body.addEventListener('mouseup',function(){
+    active = false;
+    resizer.classList.remove('resize');
+});
+
+document.body.addEventListener('mouseleave', function() {
+    active = false;
+    resizer.classList.remove('resize');
+});
+
+document.body.addEventListener('mousemove',function(e){
+    if (!active) return;
+    let x = e.pageX;
+    x -= slider.getBoundingClientRect().left;
+    slideIt(x);
+    pauseEvent(e);
+});
+
+resizer.addEventListener('touchstart',function(){
+    active = true;
+    resizer.classList.add('resize');
+});
+
+document.body.addEventListener('touchend',function(){
+    active = false;
+    resizer.classList.remove('resize');
+});
+
+document.body.addEventListener('touchcancel',function(){
+    active = false;
+    resizer.classList.remove('resize');
+});
+
+document.body.addEventListener('touchmove',function(e){
+    if (!active) return;
+    let x;
+
+    let i;
+    for (i=0; i < e.changedTouches.length; i++) {
+        x = e.changedTouches[i].pageX;
+    }
+
+    x -= slider.getBoundingClientRect().left;
+    slideIt(x);
+    pauseEvent(e);
+});
+
+function slideIt(x){
+    let transform = Math.max(0,(Math.min(x,slider.offsetWidth)));
+    before.style.width = transform+"px";
+    resizer.style.left = transform-0+"px";
+}
+
+function pauseEvent(e){
+    if(e.stopPropagation) e.stopPropagation();
+    if(e.preventDefault) e.preventDefault();
+    e.cancelBubble=true;
+    e.returnValue=false;
+    return false;
+}
+
